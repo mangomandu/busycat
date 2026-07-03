0c8b8d46f973bf90877d7f818e3346e9d3519433
dlfnek <dlfnek2002@gmail.com>
2026-07-03 11:34:50 +0900

    Refresh v1.1.2 DMG checksum

--- Casks/busycat.rb
+++ Casks/busycat.rb
@@ -1,6 +1,6 @@
 cask "busycat" do
   version "1.1.2"
-  sha256 "7e623869758dbd910adce5f9f2ae551647c6e0993d982fb924a3ed02d4f5f63e"
+  sha256 "58d541f8dbb3fd7d1638bf24faf971508624d2045e01e5110e54854240b32519"
 
   url "https://github.com/mangomandu/busycat/releases/download/v#{version}/BusyCat-#{version}-macOS.dmg"
   name "BusyCat"
