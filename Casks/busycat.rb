bd0929ff08ffd3d603d9b737b15593cae190cda9
dlfnek <dlfnek2002@gmail.com>
2026-07-12 13:13:35 +0900

    Prepare v1.1.4 release

--- Casks/busycat.rb
+++ Casks/busycat.rb
@@ -1,19 +1,19 @@
 cask "busycat" do
-  version "1.1.3"
-  sha256 "dc80c3bf1df927d35d5e551c92ef72adf59de81232363b60a78fa66825dfa164"
+  version "1.1.4"
+  sha256 "e4831e398fc6340b744e6110353755a015a36edf9fe4479507c3825b834904d0"
 
   url "https://github.com/mangomandu/busycat/releases/download/v#{version}/BusyCat-#{version}-macOS.dmg"
   name "BusyCat"
   desc "Menu bar cat driven by CPU and GPU compute load"
   homepage "https://github.com/mangomandu/busycat"
 
-  depends_on arch: :arm64
-  depends_on macos: :ventura
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  depends_on arch: :arm64
+  depends_on macos: :ventura
 
   app "BusyCat.app"
 
--- make_dmg.sh
+++ make_dmg.sh
@@ -23,7 +23,7 @@ fi
 
 ./make_app.sh
 
-if ! lipo -verify_arch arm64 "$APP_BUNDLE/Contents/MacOS/$APP_NAME"; then
+if ! lipo "$APP_BUNDLE/Contents/MacOS/$APP_NAME" -verify_arch arm64; then
     echo "BusyCat releases must contain an arm64 binary." >&2
     exit 1
 fi
